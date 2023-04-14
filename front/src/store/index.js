79b6c00e2b1b820847d9e83b754777e94e2d326c
woo dahye <dahye.woo@inthekeg.com>
2023-04-14 17:29:53 +0900

    add user token state in vuex

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -4,7 +4,27 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: {
+      accToken: null,
+      accTokenExp: null,
+      uid: "",
+    },
+  },
+  getters: {
+    userInfo(state) {
+      return state.user;
+    },
+    userAccTokenExp: (state) => {
+      return state.user.accTokenExp;
+    },
+    userAccToken: (state) => {
+      return state.user.accToken;
+    },
+    userUID: (state) => {
+      return state.user.userUID;
+    },
+  },
   mutations: {},
   actions: {},
   modules: {},
