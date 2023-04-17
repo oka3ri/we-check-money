3376c6d2af3c13d06f468776c5993d96290055e7
woo dahye <dahye.woo@inthekeg.com>
2023-04-17 18:13:57 +0900

    add kakao login logic

--- front/src/main.js
+++ front/src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import VueSimpleAlert from "vue-simple-alert";
 import DashboardPlugin from "./plugins/dashboard-plugin";
 import RouterPushPlugIn from "./plugins/routerPush";
-import ServerAPI from "./plugins/serverAPI";
 import App from "./App.vue";
 import router from "./router/router";
 import store from "./store";
@@ -10,12 +9,13 @@ import store from "./store";
 // Vue.config.productionTip = false;
 Vue.use(DashboardPlugin);
 Vue.use(RouterPushPlugIn);
-Vue.use(ServerAPI);
 Vue.use(VueSimpleAlert, {
   customClass: {
     container: "vue-simple-alert-container",
   },
 });
+
+window.Kakao.init("a31431011cd4fbc7a117ec76d269aee3");
 
 new Vue({
   router,
